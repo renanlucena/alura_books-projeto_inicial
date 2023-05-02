d76b03acaf1b46d11663f95e73de036d12eefc6a
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-02 18:02:36 -0300

    implementando o método Sort

---
+++ app/metodoSort.js
@@ -0,0 +1,7 @@
+let btnOrdenar = document.getElementById('btnOrdenarPorPreco');
+btnOrdenar.addEventListener ('click', ordenarLivrosPreco );
+
+function ordenarLivrosPreco(){
+    let livrosOrdenados = livros.sort((a, b) => a.preco - b.preco );
+    exibirOsLivrosNaTela(livrosOrdenados);
+}
