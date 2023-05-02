32dec086e83b1eb0ec7c20facf73610f6a8c7b22
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-02 15:03:52 -0300

    consumindo dados de uma API externa

--- app/main.js
+++ app/main.js
@@ -1,2 +1,10 @@
 let livros = [];
 const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+
+gestBuscarLivrosAPI()
+
+async function gestBuscarLivrosAPI(){
+    const resposta = await fetch(endpointAPI);
+    livros = await resposta.json();
+    console.table(livros);
+}
