5a585101b36332e334a0d9a010a547afd732fce0
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-02 15:35:39 -0300

    separando a função exibirOsLivrosNaTela do main.js

--- app/main.js
+++ app/main.js
@@ -3,9 +3,6 @@ const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json
 
 gestBuscarLivrosAPI();
 
-const elementoParaInserirLivros = document.getElementById('livros');
-
-
 async function gestBuscarLivrosAPI(){
     const resposta = await fetch(endpointAPI);
     livros = await resposta.json();
@@ -13,20 +10,3 @@ async function gestBuscarLivrosAPI(){
     exibirOsLivrosNaTela(livros);
 }
 
-function exibirOsLivrosNaTela(listaDeLivros){
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo">
-          ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${livro.preco}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-        `
-    })
-    
-}
